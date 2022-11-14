46903d0631b49d8282257f378270055c411c1966
Xasanboy <hasanbojabdurasulov309@gmail.com>
2022-11-15 01:42:17 +0500

    Middle

---
+++ main.js
@@ -0,0 +1,26 @@
+let fal = document.querySelector(".false").innerText
+function one() {
+    document.querySelector(".false").innerText = fal += 1
+} function zero() {
+    document.querySelector(".false").innerText = fal += 0
+}
+function two() {
+    document.querySelector(".false").innerText = fal += 2
+} function three() {
+    document.querySelector(".false").innerText = fal += 3
+} function four() {
+    document.querySelector(".false").innerText = fal += 4
+} function five() {
+    document.querySelector(".false").innerText = fal += 5
+} function six() {
+    document.querySelector(".false").innerText = fal += 6
+} function seven() {
+    document.querySelector(".false").innerText = fal += 7
+} function eight() {
+    document.querySelector(".false").innerText = fal += 8
+} function nine() {
+    document.querySelector(".false").innerText = fal += 9
+} function zeros() {
+    document.querySelector(".false").innerText += "00"
+}
+let length =  document.querySelector(".true").innerText
